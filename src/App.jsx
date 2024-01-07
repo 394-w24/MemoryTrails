3ec7ed79a167bff3ad4d176fa2b2c3762ffe7109
ConnieLuksc <1155142806@link.cuhk.edu.hk>
2024-01-06 18:19:05 -0600

    Fix bug

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from "react-dom/client";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import {Homepage} from "./components/Homepage";
+import Homepage from "./components/Homepage.jsx";
 
 const App = ()  => {
   return (
