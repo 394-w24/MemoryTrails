2fda05447f88c8e2cc26f8371e114dcab326450f
Macy Bosnich <macybosnich2024@u.northwestern.edu>
2024-01-08 12:58:28 -0600

    add simple homepage button

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,5 +1,12 @@
 import React from 'react';
 import './Banner.css'; 
+import { Link } from 'react-router-dom';
+
+const linkStyle = {
+  margin: "1rem",
+  textDecoration: "none",
+  color: 'black'
+};
 
 const Banner = () => {
   return (
@@ -7,6 +14,9 @@ const Banner = () => {
       <div className="banner-left">
         <img src="https://www.kindpng.com/picc/m/200-2004455_travel-icon-png-transparent-png.png" alt="Travel Icon" className="logo" />
         <span className="title">MemoryTrails</span>
+        <Link to={"/"} style={linkStyle}>
+              Homepage
+        </Link>
       </div>
       <div className="banner-right">
         <button className="signin-button">Sign In</button>
