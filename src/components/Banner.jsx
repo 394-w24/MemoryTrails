f1c0a9dcad0da8529ab17ce0b0b286a61a78980d
Macy Bosnich <macybosnich2024@u.northwestern.edu>
2024-01-12 17:59:53 -0600

    continuing add trip modal ui

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -11,6 +11,7 @@ const Banner = () => {
   const [show, setShow] = useState(false);
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+  
   return (
     <div className="banner">
       <div className="banner-left">
@@ -36,7 +37,6 @@ const Banner = () => {
                 <Form.Control
                   type="trip_name"
                   placeholder="Your Trip's name"
-                  autoFocus
                 />
               </Form.Group>
               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
@@ -47,10 +47,18 @@ const Banner = () => {
                 <Form.Label>Trip date:</Form.Label>
                 <Form.Control as="textarea" rows={1} />
               </Form.Group>
-              <div class="mb-3">
-                <label for="formFileMultiple" class="form-label">Multiple files input example</label>
-                <input class="form-control" type="file" id="formFileMultiple" multiple />
-              </div>
+              <hr/>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
+                <Form.Label>Location</Form.Label>
+                <Form.Control type = "location" as="textarea" rows={1} />
+                <div class="mb-3">
+                  <label for="formFileMultiple" class="form-label">Multiple files input example</label>
+                  <input class="form-control" type="file" id="formFileMultiple" multiple />
+                </div>
+                <Form.Label>Caption</Form.Label>
+                <Form.Control type = "caption" as="textarea" rows={1} />
+              </Form.Group>
+              <hr/>
             </Form>
           </Modal.Body>
           <Modal.Footer>
