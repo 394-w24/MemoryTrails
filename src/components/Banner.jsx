a19040029e22d30688b048bff6ea8dddda6aad98
Jin <zhan7075@umn.edu>
2024-01-12 17:12:57 -0600

    adding modal

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -1,8 +1,16 @@
 import React from 'react';
+import { useState } from 'react';
 import './Banner.css'; 
 import { Link } from 'react-router-dom';
+import Button from 'react-bootstrap/Button';
+import Form from 'react-bootstrap/Form';
+import Modal from 'react-bootstrap/Modal';
+
 
 const Banner = () => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
   return (
     <div className="banner">
       <div className="banner-left">
@@ -11,6 +19,44 @@ const Banner = () => {
         <Link to={"/"} className='link-style' >
               Homepage
         </Link>
+      </div>
+
+      <div className='banner-right'>
+        <Button variant="primary" onClick={handleShow}>
+          Add Trip
+        </Button>
+        <Modal show={show} onHide={handleClose}>
+          <Modal.Header closeButton>
+            <Modal.Title>Modal heading</Modal.Title>
+          </Modal.Header>
+          <Modal.Body>
+            <Form>
+              <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                <Form.Label>Email address</Form.Label>
+                <Form.Control
+                  type="email"
+                  placeholder="name@example.com"
+                  autoFocus
+                />
+              </Form.Group>
+              <Form.Group
+                className="mb-3"
+                controlId="exampleForm.ControlTextarea1"
+              >
+                <Form.Label>Example textarea</Form.Label>
+                <Form.Control as="textarea" rows={3} />
+              </Form.Group>
+            </Form>
+          </Modal.Body>
+          <Modal.Footer>
+            <Button variant="secondary" onClick={handleClose}>
+              Close
+            </Button>
+            <Button variant="primary" onClick={handleClose}>
+              Save Changes
+            </Button>
+          </Modal.Footer>
+        </Modal>
       </div>
     </div>
   );
