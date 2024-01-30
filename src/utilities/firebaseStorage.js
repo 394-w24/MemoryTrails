c37bfe12987c584699787396c4242ca89e1e110a
Jin <zhan7075@umn.edu>
2024-01-29 19:53:08 -0600

    Fixing photo problem, need install uuid, same location two photo still doesn't work

--- src/utilities/firebaseStorage.js
+++ src/utilities/firebaseStorage.js
@@ -1,21 +1,44 @@
-import { getStorage, ref, uploadBytes, getDownloadURL  } from 'firebase/storage';
+import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 
 export const uploadFileToFirebase = async (file, locationName, id) => {
     if (!file) {
         throw new Error("No file provided");
     }
+
     const storage = getStorage();
-    console.log("storage:",storage)
-    console.log("filename:",file.name)
-    const fileRef = ref(storage, `TripImages/${locationName}_${id}`);
-    console.log("storageref:",fileRef)
-  
-    try {
-      const snapshot = await uploadBytes(fileRef, file);
-      const url = await getDownloadURL(snapshot.ref);
-      return url;
-    } catch (error) {
-        console.error("Error uploading file: ", error);
-        throw error;
+    let fileName = `${locationName}_${id}`;
+    let fileRef = ref(storage, `TripImages/${fileName}`);
+    let url;
+    let count = 0;
+
+    while (true) {
+        try {
+            // Try to get the download URL of the file
+            url = await getDownloadURL(fileRef);
+            console.log('already in firebase');
+
+            // File exists, append a number to the file name and try again
+            count++;
+            fileName = `${locationName}_${id}_${count}`;
+            fileRef = ref(storage, `TripImages/${fileName}`);
+        } catch (error) {
+            // If error is 'storage/object-not-found', then file does not exist, and we can upload it
+            if (error.code === 'storage/object-not-found') {
+                try {
+                    const snapshot = await uploadBytes(fileRef, file);
+                    url = await getDownloadURL(snapshot.ref);
+                    console.log("File uploaded successfully: ", fileName);
+                    break;
+                } catch (uploadError) {
+                    console.error("Error uploading file: ", uploadError);
+                    throw uploadError;
+                }
+            } else {
+                console.error("Error checking file existence: ", error);
+                throw error;
+            }
+        }
     }
-  };
+
+    return url;
+};
