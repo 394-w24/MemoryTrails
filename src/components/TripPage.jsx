be4c904f6782e83fdd7de6819145ac5ded0ab7cd
Macy Bosnich <macybosnich2024@u.northwestern.edu>
2024-02-02 16:40:29 -0600

    fix markers moving on zoom

--- src/components/TripPage.jsx
+++ src/components/TripPage.jsx
@@ -6,19 +6,8 @@ import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
 import "leaflet/dist/leaflet.css";
 import L from "leaflet";
 import icon from "leaflet/dist/images/marker-icon.png";
-import iconRetina from "leaflet/dist/images/marker-icon-2x.png";
-import iconShadow from "leaflet/dist/images/marker-shadow.png";
 import { useDbData } from "../utilities/firebase";
 
-const customIcon = new L.Icon({
-  iconUrl: icon,
-  iconRetinaUrl: iconRetina,
-  shadowUrl: iconShadow,
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41],
-});
 
 const numberedIcon = (number) => new L.DivIcon({
   className: "custom-icon",
