999d8309c05e3aa06346d8ac79d463c60204bbc9
Macy Bosnich <macybosnich2024@u.northwestern.edu>
2024-02-02 17:50:26 -0600

    sort trips by order

--- src/components/homepage.jsx
+++ src/components/homepage.jsx
@@ -6,17 +6,18 @@ import { useDbData } from "../utilities/firebase.js";
 
 const HomePage = () => {
   const tripData = useDbData("trips/");
-
+  //.sort((a, b) => new Date(a[1]?.locations[0]?.date) - new Date(b[1]?.locations[0]?.date))
   if (tripData[0] === undefined) {
     return <div></div>;
   }
+  console.log("tripDate:", tripData)
   return (
     <div>
       <div>
         <MapPage />
       </div>
       <div className="trip-list">
-        {Object.entries(tripData[0]).map(([tripId, trip]) => (
+        {Object.entries(tripData[0]).sort((a, b) => new Date(b[1]?.locations[0]?.date).getTime() - new Date(a[1]?.locations[0]?.date).getTime()).map(([tripId, trip]) => (
           <Trip key={tripId} tripId={tripId} trip={trip} />
         ))}
       </div>
